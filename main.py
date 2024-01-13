55561898d216ab3408bbed09e44c8f6bb2b820fe
Ваше Имя <you@example.com>
2024-01-13 23:47:07 +0300

    arstarstarst123arsarstatarstarst

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,7 +18,8 @@ name_file = "img.png"
 
 options = Options()
 options.add_argument("-headless")
-driver = webdriver.Firefox(options=options)
+#driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox()
 
 driver.maximize_window()
 driver.implicitly_wait(15)
@@ -36,39 +39,56 @@ if "verified-device" in driver.current_url:
 
     driver.get("https://account.mail.ru/login")
     sleep(10)
-    driver.find_element(By.XPATH, "//input").send_keys(MAIL)
-    driver.save_screenshot(name_file)
-    files = {'photo': open(name_file, 'rb')}
-    print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
-    driver.find_element(By.XPATH, "//button/span").click()
-    sleep(5)
-    driver.find_element(By.XPATH, "//input[@name='password']").send_keys(PASSWORD_MAIL)
-    driver.find_element(By.XPATH, "//span").click()
-    sleep(5)
-    driver.get("https://e.mail.ru/search/?q_query=GitHub")
-    sleep(5)
-    driver.save_screenshot(name_file)
-    files = {'photo': open(name_file, 'rb')}
-    print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
-    driver.find_element(By.XPATH, "//span[2]/div/span/span/span[3]").click()
-    sleep(1)
-    data = driver.page_source
-    code = re.findall(r'Verification code: [\d]+<br>', data)
-    code = code[0].split()[-1].replace("<br>", "")
-    driver.get("https://github.com/sessions/verified-device")
-    sleep(3)
-    driver.find_element(By.NAME, "otp").send_keys(code)
-    driver.save_screenshot(name_file)
-    files = {'photo': open(name_file, 'rb')}
+    script = f'document.querySelectorAll("input")[0].value = {MAIL}'
+    driver.execute(script)
 
-    print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
-    print(code)
-driver.get("https://account.habr.com/login/?consumer=career&state=bslogin")
-sleep(3)
-driver.find_element(By.CSS_SELECTOR, ".socials-buttons__button_github > svg").click()
-sleep(5)
-driver.save_screenshot(name_file)
-files = {'photo': open(name_file, 'rb')}
-print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+#     driver.save_screenshot(name_file)
+#     files = {'photo': open(name_file, 'rb')}
+#     print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+#     driver.find_element(By.XPATH, "//button/span").click()
+#     sleep(5)
+#     driver.find_element(By.XPATH, "//input[@name='password']").send_keys(PASSWORD_MAIL)
+#     driver.find_element(By.XPATH, "//span").click()
+#     sleep(5)
+#     driver.get("https://e.mail.ru/search/?q_query=GitHub")
+#     sleep(5)
+#     driver.save_screenshot(name_file)
+#     files = {'photo': open(name_file, 'rb')}
+#     print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+#     driver.find_element(By.XPATH, "//span[2]/div/span/span/span[3]").click()
+#     sleep(1)
+#     data = driver.page_source
+#     code = re.findall(r'Verification code: [\d]+<br>', data)
+#     code = code[0].split()[-1].replace("<br>", "")
+#     driver.get("https://github.com/sessions/verified-device")
+#     sleep(3)
+#     driver.find_element(By.NAME, "otp").send_keys(code)
+#     driver.save_screenshot(name_file)
+#     files = {'photo': open(name_file, 'rb')}
+#
+#     print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+#     print(code)
+# driver.get("https://account.habr.com/login/?consumer=career&state=bslogin")
+# sleep(3)
+# driver.find_element(By.CSS_SELECTOR, ".socials-buttons__button_github > svg").click()
+# sleep(5)
+# driver.save_screenshot(name_file)
+# files = {'photo': open(name_file, 'rb')}
+# print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+#
+# driver.close()
 
-driver.close()
+# driver.get("https://account.mail.ru/login")
+# sleep(10)
+# script = f'document.querySelectorAll("input")[0].value = {MAIL}'
+# driver.execute(script)
+# driver.save_screenshot(name_file)
+# files = {'photo': open(name_file, 'rb')}
+# print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+# driver.find_element(By.XPATH, "//button/span").click()
+# sleep(5)
+# driver.find_element(By.XPATH, "//input[@name='password']").send_keys(PASSWORD_MAIL)
+# driver.find_element(By.XPATH, "//span").click()
+# sleep(5)
+# driver.get("https://e.mail.ru/search/?q_query=GitHub")
+# sleep(5)
