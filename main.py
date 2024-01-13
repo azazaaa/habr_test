4cb23d53d5bda99d5f0fd99d98522d2722c5e3be
Ваше Имя <you@example.com>
2024-01-13 22:38:18 +0300

    arstarstarst123arsarstat

--- main.py
+++ main.py
@@ -4,35 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 import re
-import sys
 import os
-value = os.environ.get("MAIL")
-test = os.environ.get("TEST")
-print(value,test)
 
-
-# LOGIN = sys.argv[1]
-# PASSWORD = sys.argv[2]
-TOKEN = sys.argv[1]
-# TOKEN = sys.argv[3]
-
+MAIL = os.environ.get("MAIL")
+PASSWORD_GIT = os.environ.get("PASSWORD_GIT")
+PASSWORD_MAIL = os.environ.get("PASSWORD_MAIL")
+TOKEN = os.environ.get("TOKEN")
 CHAT_ID = 836801516
-# name_file = "img.png"
-#
+
+name_file = "img.png"
+
 options = Options()
 options.add_argument("-headless")
 driver = webdriver.Firefox(options=options)
-#driver = webdriver.Firefox()
+
 driver.maximize_window()
 driver.implicitly_wait(10)
-
-driver.get("https://account.mail.ru/login")
-sleep(5)
-driver.find_element(By.XPATH, "//input[@name='username']").send_keys(value)
-
-driver.save_screenshot("name_file")
-files = {'photo': open("name_file", 'rb')}
-print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
 
 #
 # driver.get("https://account.mail.ru/login")
@@ -59,12 +46,21 @@ print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHA
 # with open("text.txt", "w") as f:
 #     f.writelines(html)
 
-# driver.get("https://github.com/login")
-# sleep(3)
-# driver.find_element(By.ID, "login_field").send_keys(LOGIN)
-# driver.find_element(By.ID, "password").send_keys(PASSWORD)
-# driver.find_element(By.NAME, "commit").click()
-# sleep(5)
+driver.get("https://github.com/login")
+sleep(3)
+driver.find_element(By.ID, "login_field").send_keys(PASSWORD_GIT)
+driver.find_element(By.ID, "password").send_keys(PASSWORD_MAIL)
+driver.find_element(By.NAME, "commit").click()
+sleep(5)
+
+html = driver.page_source
+
+with open("text.txt", "w") as f:
+    f.writelines(html)
+
+files = {'document': open("text.txt", 'rb')}
+print(requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}', files=files).json())
+
 # if "verified-device" in driver.current_url:
 #     driver.get("https://mail.ru/")
 #     driver.find_element(By.CSS_SELECTOR, ".resplash-btn_primary").click()
